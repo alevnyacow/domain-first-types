da7c2219bf1f09669fcea891689aab3f1401da68
alevny <alevnyacow@gmail.com>
2026-07-23 20:32:53 +0400

    fix: darker logo, removed unused error

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,3 +1,2 @@
 export * from './async-schema-in-sync-parsing-error';
 export * from './invalid-data-parsing-error';
-export * from './structured-clone-not-found-error';
--- src/errors/structured-clone-not-found-error.ts
+++
@@ -1,5 +0,0 @@
-import { defineErrorClass } from '@domain-first/errors';
-
-export const StructuredCloneNotFoundError = defineErrorClass({
-    code: 'STRUCTURED_CLONE_NOT_FOUND'
-});
