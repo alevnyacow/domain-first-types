ce4e00c3fc1932e171d6fd407377bee907bfdd23
alevnyacow <75858769+alevnyacow@users.noreply.github.com>
2026-07-27 15:29:51 +0400

    fix: value object tests with `zod`, `valibot`, `joi`

--- src/value-object.spec.ts
+++ src/value-object.spec.ts
@@ -1,52 +1,131 @@
 import { expect, test } from '@rstest/core';
+import Joi from 'joi';
+import * as v from 'valibot';
 import z from 'zod';
+
 import { InvalidDataParsingError } from './errors';
 import { defineValueObject } from './value-object';
 
-class Pagination extends defineValueObject(
-    z.object({
-        pageSize: z.int().positive(),
-        pageIndex: z.int().positive()
-    })
-) {
-    get nextPage() {
-        return new Pagination({
-            ...this.model,
-            pageIndex: this.model.pageIndex + 1
-        });
-    }
+test('zod', () => {
+    class ZodNonEmptyString extends defineValueObject(z.string().nonempty()) {}
 
-    get previousPage() {
-        return new Pagination({
-            ...this.model,
-            pageIndex: this.model.pageIndex - 1
-        });
-    }
-}
+    const nonEmptyString = new ZodNonEmptyString('non-empty');
+    expect(nonEmptyString.model).toBe('non-empty');
 
-test('d', () => {
-    const secondPage = new Pagination({
-        pageIndex: 2,
-        pageSize: 10
+    expect(() => new ZodNonEmptyString('')).toThrowError(
+        InvalidDataParsingError
+    );
+
+    class ZodUserData extends defineValueObject(
+        z.object({
+            firstName: z.instanceof(ZodNonEmptyString),
+            lastName: z.instanceof(ZodNonEmptyString),
+            age: z.int().positive()
+        })
+    ) {}
+
+    const userData = new ZodUserData({
+        firstName: new ZodNonEmptyString('John'),
+        lastName: new ZodNonEmptyString('Doe'),
+        age: 30
     });
 
-    const a = null as any;
-    if (a instanceof Pagination) {
-        a.nextPage.model;
-    }
-
-    const firstPage = secondPage.previousPage;
-    expect(firstPage.model).toStrictEqual({
-        pageIndex: 1,
-        pageSize: 10
+    expect(userData.model).toEqual({
+        firstName: expect.any(ZodNonEmptyString),
+        lastName: expect.any(ZodNonEmptyString),
+        age: 30
     });
-    try {
-        const _invalidZeroPage = firstPage.previousPage;
-    } catch (e: unknown) {
-        if (InvalidDataParsingError.is(e)) {
-            console.error('Invalid data!');
-            console.error(e);
-        }
-    }
-    expect(true).toBe(true);
+
+    expect(
+        () =>
+            new ZodUserData({
+                firstName: new ZodNonEmptyString('John'),
+                lastName: new ZodNonEmptyString('Doe'),
+                age: -30
+            })
+    ).toThrowError(InvalidDataParsingError);
+});
+
+test('valibot', () => {
+    class ValibotNonEmptyString extends defineValueObject(
+        v.pipe(v.string(), v.nonEmpty())
+    ) {}
+
+    const nonEmptyString = new ValibotNonEmptyString('non-empty');
+    expect(nonEmptyString.model).toBe('non-empty');
+
+    expect(() => new ValibotNonEmptyString('')).toThrowError(
+        InvalidDataParsingError
+    );
+
+    class ValibotUserData extends defineValueObject(
+        v.object({
+            firstName: v.instance(ValibotNonEmptyString),
+            lastName: v.instance(ValibotNonEmptyString),
+            age: v.pipe(v.number(), v.integer(), v.minValue(1))
+        })
+    ) {}
+
+    const userData = new ValibotUserData({
+        firstName: new ValibotNonEmptyString('John'),
+        lastName: new ValibotNonEmptyString('Doe'),
+        age: 30
+    });
+
+    expect(userData.model).toEqual({
+        firstName: expect.any(ValibotNonEmptyString),
+        lastName: expect.any(ValibotNonEmptyString),
+        age: 30
+    });
+
+    expect(
+        () =>
+            new ValibotUserData({
+                firstName: new ValibotNonEmptyString('John'),
+                lastName: new ValibotNonEmptyString('Doe'),
+                age: -30
+            })
+    ).toThrowError(InvalidDataParsingError);
+});
+
+test('joi', () => {
+    class JoiNonEmptyString extends defineValueObject(
+        Joi.string().min(1).required()
+    ) {}
+
+    const nonEmptyString = new JoiNonEmptyString('non-empty');
+    expect(nonEmptyString.model).toBe('non-empty');
+
+    expect(() => new JoiNonEmptyString('')).toThrowError(
+        InvalidDataParsingError
+    );
+
+    class JoiUserData extends defineValueObject(
+        Joi.object({
+            firstName: Joi.object().instance(JoiNonEmptyString).required(),
+            lastName: Joi.object().instance(JoiNonEmptyString).required(),
+            age: Joi.number().integer().positive().required()
+        })
+    ) {}
+
+    const userData = new JoiUserData({
+        firstName: new JoiNonEmptyString('John'),
+        lastName: new JoiNonEmptyString('Doe'),
+        age: 30
+    });
+
+    expect(userData.model).toEqual({
+        firstName: expect.any(JoiNonEmptyString),
+        lastName: expect.any(JoiNonEmptyString),
+        age: 30
+    });
+
+    expect(
+        () =>
+            new JoiUserData({
+                firstName: new JoiNonEmptyString('John'),
+                lastName: new JoiNonEmptyString('Doe'),
+                age: -30
+            })
+    ).toThrowError(InvalidDataParsingError);
 });
